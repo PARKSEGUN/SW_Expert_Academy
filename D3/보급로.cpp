71cb43abda4e9cd0a325284140c1ddedc1f9dc15
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2024-01-10 23:16:11 +0900

    Update 보급로.cpp

--- D3/보급로.cpp
+++ D3/보급로.cpp
@@ -4,6 +4,10 @@
 DPS로 해결하려했지만 시간초과가 발생했고 DPS를 구현하면서 DPS와 비슷한 로직으로 DP를 구현해 볼 수 있을 것 같아 구현해서 시간을 줄일 수 있었다.
 하지만 이 또한 시간초과가 발생했고 원하는 풀이법이 아니라고 생각했다.
 모든 경우를 확인하는 또 다른 알고리즘으로 BFS를 생각했고 구현해서 해결하였다.
+하지만 다른 사람들의 결과를 보고 내 코드가 속도가 많이 느리다고 생각했고
+이유를 찾던중 sumQueue라는 것을 구현한 부분에서 차이가있었다.
+왜 sum으로 조건을 거는것과 dp를 사용해서 걸어주는 것에 속도차이가 있을까 생각하던중에 sum으로 조건을 걸게된다면 다음 큐에서 나올때 이전에 큐에 넣어주었던 값이 그대로 나오지만
+dp를 조건으로 사용해준다면 queue에 넣어주는 것과 상관없이 더 낮은 값으로 계속 초기화 될 수 있었고 이에 속도를 줄일 수 있었다.
 
   -------------------------------------DP로 시도했던 코드----------------------------
   #include<iostream>
@@ -148,3 +152,79 @@ int main(void) {
 	}
 }
   
+------------sumQueue를 제거하고 dp를 기준으로 설정해서 최적화---------------
+	#include<iostream>
+#include <vector>
+#include<cstring>
+#include<climits>
+#include<queue>
+
+using namespace std;
+
+int dix[4] = { 0,0,1,-1 };
+int diy[4] = { 1,-1,0,0 };
+int answer2 = 0;
+void inputToMap(int N, int map[][111]) {
+	for (int i = 0; i < N; i++) {
+		string str;
+		cin >> str;
+		for (int j = 0; j < N; j++) {
+			map[i][j] = str[j] - '0';
+		}
+	}
+}
+
+
+
+void settingDpArr(int N, int dp[][111]) {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			dp[i][j] = INT_MAX;
+		}
+	}
+}
+
+void findMinValue(int xx, int yy, int N, int map[][111], int dp[][111]) {
+	queue<pair<int, int>> pointQueue;
+	queue<int> sumQueue;
+	pointQueue.push({ xx,yy });
+	while (pointQueue.size() > 0) {
+		int x = pointQueue.front().first;
+		int y = pointQueue.front().second;
+		pointQueue.pop();
+		for (int i = 0; i < 4; i++) {
+			int dx = x + dix[i];
+			int dy = y + diy[i];
+			if (dx >= 0 && dx < N && dy >= 0 && dy < N) {
+				if (dp[x][y] + map[dx][dy] < dp[dx][dy]) {
+					dp[dx][dy] = dp[x][y] + map[dx][dy];
+					pointQueue.push({ dx,dy });
+				}
+			}
+		}
+	}
+
+}
+
+void solve(int T) {
+	int N;
+	cin >> N;
+	int map[111][111];
+	int dp[111][111];
+	inputToMap(N, map);
+	settingDpArr(N, dp);
+	dp[0][0] = 0;
+	findMinValue(0, 0, N, map, dp);
+	cout << "#" << T << " " << dp[N - 1][N - 1] << endl;
+}
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int T;
+	cin >> T;
+	for (int i = 1; i <= T; i++) {
+		solve(i);
+	}
+}
